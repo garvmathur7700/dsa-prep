ae672e1e1b7e2e9250d22c760274e0d5b91f096c
Garv <garvmathur7700@gmail.com>
2025-11-01 21:05:54 +0530

    better way

--- step3/hard/MajorityElement2.java
+++ step3/hard/MajorityElement2.java
@@ -11,6 +11,24 @@ public class MajorityElement2 {
     }
 
     public static List<Integer> majorityElement (int[] nums) {
+        int n = nums.length / 3;
+        Map<Integer, Integer> mpp = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+        Set<Integer> resultSet = new HashSet<>();
 
+        for (int num : nums) {
+            int freq = 1;
+            if (mpp.containsKey(num)) {
+                freq = mpp.get(num);
+                freq++;
+            }
+            mpp.put(num, freq);
+            if (freq > n && !resultSet.contains(num)) {
+                resultSet.add(num);
+                result.add(num);
+            }
+        }
+
+        return result;
     }
 }
