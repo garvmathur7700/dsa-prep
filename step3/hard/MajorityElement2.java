24b319c9a0b3fe55a5790035af9b6e5c8837cae0
Garv <garvmathur7700@gmail.com>
2025-11-03 21:33:15 +0530

    added optimal method - Moores voting algorithm

--- step3/hard/MajorityElement2.java
+++ step3/hard/MajorityElement2.java
@@ -6,8 +6,8 @@ import java.util.*;
 
 public class MajorityElement2 {
     public static void main(String[] args) {
-        int[] nums = { 2, 2 };
-        System.out.println("Majority Element: " + majorityElement(nums));
+        int[] nums = { 2, 1, 1, 3, 1, 4, 5, 6 };
+        System.out.println("Majority Element: " + majEleOptimal(nums));
     }
 
     public static List<Integer> majorityElement (int[] nums) {
@@ -29,5 +29,43 @@ public class MajorityElement2 {
             }
         }
         return result;
+    }
+
+    public static List<Integer> majEleOptimal (int[] nums) {
+        int count1 = 0;
+        int count2 = 0;
+        int el1 = Integer.MIN_VALUE;
+        int el2 = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count1 == 0 && el2 != nums[i]) {
+                count1 = 1;
+                el1 = nums[i];
+            } else if (count2 == 0 && el1 != nums[i]) {
+                count2 = 1;
+                el2 = nums[i];
+            } else if (el1 == nums[i]) count1++;
+            else if (el2 == nums[i]) count2++;
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        List<Integer> results = new ArrayList<>();
+        /**
+         * The counters have to be reset because they only indicate the final candidates in the Moore's Voting Algorithm.
+         */
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (el1 == nums[i]) count1++;
+            if (el2 == nums[i]) count2++;
+        }
+
+        if (count1 > nums.length / 3) results.add(el1);
+        if (count2 > nums.length / 3) results.add(el2);
+
+        return results;
     }
 }
