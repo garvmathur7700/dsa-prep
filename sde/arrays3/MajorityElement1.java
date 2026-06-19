869d6a686f2607d685d891b5ac6d9a4d5f9f879e
garvmathur7700 <garvmathur7700@gmail.com>
2026-06-19 07:43:04 +0530

    sde: 2 arrays3

---
+++ sde/arrays3/MajorityElement1.java
@@ -0,0 +1,18 @@
+class MajorityElement1 {
+    public int majorityElement(int[] nums) {
+        int element = Integer.MIN_VALUE;
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                element = num;
+                count++;
+            }
+            else if (num == element)
+                count++;
+            else
+        }
+
+        return element;
+    }
+}
---
+++ sde/arrays3/MajorityElement2.java
@@ -0,0 +1,38 @@
+class MajorityElement2 {
+    public List<Integer> majorityElement(int[] nums) {
+        int ele1 = Integer.MIN_VALUE;
+        int cnt1 = 0;
+        int ele2 = Integer.MIN_VALUE;
+        int cnt2 = 0;
+
+        for (int num : nums) {
+            if (cnt1 == 0 && ele2 != num) {
+                ele1 = num;
+                cnt1++;
+            } else if (cnt2 == 0 && ele1 != num) {
+                ele2 = num;
+                cnt2++;
+            } else if (num == ele1)
+                cnt1++;
+            else if (num == ele2) 
+                cnt2++;
+            else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int num : nums) {
+            if (num == ele1) cnt1++;
+            if (num == ele2) cnt2++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        if (cnt1 > nums.length / 3) result.add(ele1);
+        if (cnt2 > nums.length / 3) result.add(ele2);
+
+        return result;
+    }
+}
