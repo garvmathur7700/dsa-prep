36eaea93b75abd9457cff7038f7a1150e4b9dbf1
garvmathur7700 <garvmathur7700@gmail.com>
2026-07-03 11:22:03 +0530

    Binary Search: floor and ceil

---
+++ a2z/binarysearch/FloorAndCeil.java
@@ -0,0 +1,37 @@
+package binarysearch;
+
+import java.util.Arrays;
+
+public class FloorAndCeil {
+    public static void main(String[] args) {
+        int[] nums = { 2, 4, 6, 8, 10 };
+        int x = 1;
+
+        System.out.println(Arrays.toString(floorAndCeil(nums, x)));
+    }
+
+    public static int[] floorAndCeil (int[] nums, int x) {
+        int low = 0, high = nums.length - 1;
+        int floorIdx = -1;
+        int ceilIdx = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (x == nums[mid]) {
+                return new int[] { nums[mid], nums[mid] };
+            } else if (x > nums[mid]) {
+                floorIdx = mid;
+                low = mid + 1;
+            } else {
+                ceilIdx = mid;
+                high = mid - 1;
+            }
+        }
+
+        int floorVal = floorIdx == -1 ? -1 : nums[floorIdx];
+        int ceilVal = ceilIdx == -1 ? -1 : nums[ceilIdx];
+
+        return new int[] {floorVal, ceilVal};
+    }
+}
