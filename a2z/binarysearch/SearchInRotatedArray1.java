f5da482ab4e7d53902d3bc7c6b44b335a6f0fec0
garvmathur7700 <garvmathur7700@gmail.com>
2026-07-05 23:01:58 +0530

    Binary Search: search in rotated array

---
+++ a2z/binarysearch/SearchInRotatedArray1.java
@@ -0,0 +1,69 @@
+package binarysearch;
+
+public class SearchInRotatedArray1 {
+  public static void main(String[] args) {
+    int[] nums = { 5, 1, 3 };
+    int target = 1;
+
+    System.out.println("Target exists at index: " + search(nums, target));
+  }
+
+  public static int search(int[] nums, int target) {
+    if (nums.length == 1) {
+      if (nums[0] == target)
+        return 0;
+      else
+        return -1;
+    }
+
+    if (nums[0] > nums[nums.length - 1]) {
+      int pivot = findPivot(nums);
+
+      int answer = Math.max(binarySearch(nums, target, 0, pivot - 1),
+          binarySearch(nums, target, pivot, nums.length - 1));
+
+      return answer;
+    }
+
+    return binarySearch(nums, target, 0, nums.length - 1);
+  }
+
+  private static int findPivot(int[] nums) {
+    int low = 0, high = nums.length - 1;
+
+    if (nums.length == 1)
+      return 0;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+
+      if (mid < high && nums[mid] > nums[mid + 1])
+        return mid;
+      if (mid > low && nums[mid] < nums[mid - 1])
+        return mid - 1;
+      if (nums[mid] <= nums[low])
+        high = mid - 1;
+      else
+        low = mid + 1;
+    }
+
+    return -1;
+  }
+
+  private static int binarySearch(int[] nums, int target, int start, int end) {
+    int low = start, high = end;
+
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+
+      if (nums[mid] == target)
+        return mid;
+      else if (target < nums[mid])
+        high = mid - 1;
+      else
+        low = mid + 1;
+    }
+
+    return -1;
+  }
+}
