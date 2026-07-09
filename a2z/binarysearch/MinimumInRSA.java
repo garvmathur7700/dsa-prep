36c4eeb082a0d55a0e8c94a12ff31446cad0aaab
garvmathur7700 <garvmathur7700@gmail.com>
2026-07-09 10:27:32 +0530

    binary search: minor changes

---
+++ a2z/binarysearch/MinimumInRSA.java
@@ -0,0 +1,31 @@
+package binarysearch;
+
+public class MinimumInRSA {
+    public static void main(String[] args) {
+        int[] nums = { 3, 4, 5, 1, 2 };
+
+        System.out.println("Minimum in rotated sorted array: " + findMin(nums));
+    }
+
+    public static int findMin (int[] nums) {
+        if (nums[0] < nums[nums.length - 1])
+            return nums[0];
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid < right && nums[mid] > nums[mid + 1])
+                return nums[mid + 1];
+            if (mid > left && nums[mid] < nums[mid - 1])
+                return nums[mid];
+            if (nums[left] < nums[mid])
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+}
